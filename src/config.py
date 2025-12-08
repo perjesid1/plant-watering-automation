b2ae84325ca982def70e38ba4ebd2e0d3b53587d
Perjési Dániel <perjesi.codes@proton.me>
2025-12-09 00:23:23 +0100

    Refactor GPIO pins and add watering settings constants
    Updated GPIO pin assignments and added new constants for watering settings.

--- src/config.py
+++ src/config.py
@@ -1,23 +1,29 @@
 # --- GPIO Pins ---
-PUMP_RELAY_PIN: int = 4 #D2
-LDR_PIN: int = 5 #D1
-CSMS_PIN: int = 0 #A0
-OLED_SDA_PIN: int = 0 #D3
-OLED_SCL_PIN: int = 2 #D4
+PUMP_RELAY_PIN: int = 23 # GPIO23
+LDR_PIN: int = 19 # GPIO19
+CSMS_PIN: int = 34 # GPIO34
+OLED_SDA_PIN: int = 21 # GPIO21
+OLED_SCL_PIN: int = 22 # GPIO22
 
 # --- Timing Constants ---
-OLED_RESET_WAIT_MS: int = 100 # 0.1 sec in ms
-OLED_DISPLAY_S: int = 5 # 5 sec
-WATERING_WAIT_S: int = 3600 # 1 hour
-SLEEP_TIME_MS: float = 3600000 # 1 hour in ms
-PUMP_WATERING_S: int = 5 # 5 sec
+OLED_RESET_WAIT_MS: int = 100 # 0.1 sec in ms - How much time the microcontroller should wait in the booting period for the oled to initialize.
+OLED_DISPLAY_S: int = 3 # 3 sec - For how long the screen should display information.
+WATERING_WAIT_S: int = 3600 # 1 hour in sec - Minimum waiting time between wateing sessions of the plant
+SLEEP_TIME_MS: float = 3600000 # 1 hour in ms - How much the microcontroller should wait between checks.
+PUMP_WATERING_S: int = 5 # 5 sec - Total lenght of the watering period
+PUMP_PAUSE_S: int = 5 # 5 sec - Lenght of the pauses between watering cycles in case CONTINOUS_WATERING = false
 
+# --- Watering Settings ---
+MOISTURE_AIR: int = 2451 # CSMS reference value in air.
+MOISTURE_WATER: int = 863 # CSMS reference value in water.
+MOISTURE_PERCENTAGE_TO_WATER_AT: float = 50.0 # At what point between the MOSITURE_AIR and MOISTURE_WATER should we start watering. Expressed in percentage.
+CONTINOUS_WATERING: bool = True # True if we want to water the plant in multiple smaller water pulses. False otherwise.
+WATER_ONLY_IN_DARK: bool = True # True if we only water when it's dark. False otherwise.
+WATER_CYCLE_COUNT: int = 5 # If CONTINOUS_WATIRNG = True this number determines the number of watering cylces. The PUMP_WATERING_S will be divided by this number.
 
 # --- Other Constants ---
-I2C_FREQ: float = 400000
-OLED_WIDTH: int = 128
-OLED_HEIGHT: int = 32
-MOISTURE_THRESHOLD: int = 500
-RTC_ADDR_LAST_WATERED_START: int = 0
-RTC_ADDR_LAST_WATERED_END: int = 8
-MSG_REPEAT: int = 3
+I2C_FREQ: float = 400000 # Maximum frequency for SCL.
+OLED_WIDTH: int = 128 # Width of the OLED screen.
+OLED_HEIGHT: int = 32 # Height of the OLED screen.
+RTC_TIMESTAMP_SIZE: int = 8 # How many bites the timestamp takes up in the RTC memory
+MSG_REPEAT: int = 3 # How many times do we repeat the messages on the OLED screen.
