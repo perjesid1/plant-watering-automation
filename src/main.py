62ffb0eced29299b8027db60b723f56dff009961
Perjési Dániel <perjesi.codes@proton.me>
2025-12-09 00:22:21 +0100

    Enhance type hints and clarity in main.py
    Refactor main.py to improve type hints and function clarity. Added type annotations for variables and parameters to enhance code readability and maintainability.
    Finalized plant watering logic.

--- src/main.py
+++ src/main.py
@@ -21,43 +21,74 @@ except Exception as e:
     oled = None
 
 def load_last_watered_time() -> int:
+    value: int = 0
     try:
-        data = rtc.memory()
+        data: bytes = rtc.memory()
         if data:
-            return int.from_bytes(data, 'little')
+            value = int.from_bytes(data, 'little')
+            print("'load_last_watered_time' successfully read the RTC memory.")
     except Exception:
         pass
-    return 0
+    print(f'load_last_watered_time() -> {value}')
+    return value
 
-def save_last_watered_time(timestamp) -> None:
-    data = timestamp.to_bytes(config.RTC_ADDR_LAST_WATERED_END, 'little')
+def save_last_watered_time(timestamp: int) -> None:
+    data: bytes = timestamp.to_bytes(config.RTC_TIMESTAMP_SIZE, 'little')
     rtc.memory(data)
+    print(f'last_watered_time = {timestamp}. Saved to RTC memory.')
 
-def main_loop():
-    current_time = time.time()
+def get_moisture_percentage(value: int) -> float:
+    return ((config.MOISTURE_AIR - value) / (config.MOISTURE_AIR - config.MOISTURE_WATER)) * 100
 
-    last_watered_ts = load_last_watered_time()
-    time_elapsed_s = current_time - last_watered_ts
+def main_loop() -> None:
+    current_time: int = time.time()
 
-    hours = time_elapsed_s // 3600
-    minutes = (time_elapsed_s % 3600) // 60
+    last_watered_ts: int = load_last_watered_time()
+    time_elapsed_s: int = current_time - last_watered_ts
 
-    moisture_value = csms_pin.read()
-    light_condition = 'Bright' if ldr_pin.value() == 1 else 'Dark'
+    hours: int = time_elapsed_s // 3600
+    minutes: int = (time_elapsed_s % 3600) // 60
 
-    needs_watering = False
-    if moisture_value < config.MOISTURE_THRESHOLD and time_elapsed_s > config.WATERING_WAIT_S:
-        relay_pin.value(1)
-        time.sleep(config.PUMP_WATERING_S)
-        relay_pin.value(0)
+    moisture_value: float = get_moisture_percentage(csms_pin.read())
+    light_condition: str = 'Bright' if ldr_pin.value() == 1 else 'Dark'
+
+    needs_watering: bool = True
+    if moisture_value > config.MOISTURE_PERCENTAGE_TO_WATER_AT:
+        print(f'needs_watering = False, because moisture_value > config.MOISTURE_PERCENTAGE_TO_WATER_AT ({moisture_value} > {config.MOISTURE_PERCENTAGE_TO_WATER_AT})')
+        needs_watering = False
+    elif time_elapsed_s < config.WATERING_WAIT_S:
+        print(f'needs_watering = False, because time_elapsed_s < config.WATERING_WAIT_S ({time_elapsed_s} < {config.WATERING_WAIT_S})')
+        needs_watering = False
+    elif config.WATER_ONLY_IN_DARK and light_condition == 'Bright':
+        print(f"needs_watering = False, because config.WATER_ONLY_IN_DARK = True and light_condition == 'Bright' ({config.WATER_ONLY_IN_DARK} and {light_condition})")
+        needs_watering = False
+    
+    if needs_watering:
+        if config.CONTINOUS_WATERING:
+            relay_pin.value(1)
+            print('Pump relay on. Watering...')
+            time.sleep(config.PUMP_WATERING_S)
+            relay_pin.value(0)
+        else:
+            watering_time_per_cycle_s: float = config.PUMP_WATERING_S / config.WATER_CYCLE_COUNT
+            for i in range(config.WATER_CYCLE_COUNT):
+                relay_pin.value(1)
+                print('Pump relay on. Watering...')
+                time.sleep(watering_time_per_cycle_s)
+                relay_pin.value(0)
+                if i < config.WATER_CYCLE_COUNT - 1:
+                    print('Pump relay off. Waiting...')
+                time.sleep(config.PUMP_PAUSE_S)
+        
+        print('Pump relay off. Watering complete.')
         save_last_watered_time(current_time)
         last_watered_ts = current_time
-        time_elapsed_s = 0
+        time_elapsed = 0
         hours = 0
         minutes = 0
-        needs_watering = True
     
     if oled:
+        print('Oled display found. Displaying relevant information.')
         for i in range(config.MSG_REPEAT):
             oled.fill(0)
             oled.text('Last watered:', 0, 0)
@@ -91,7 +122,6 @@ def main_loop():
         oled.show()
     
     print(f'Going to sleep for {config.SLEEP_TIME_MS // 1000} seconds...')
-    print(f'Last Watered TS: {last_watered_ts}')
 
     machine.deepsleep(config.SLEEP_TIME_MS)
 
