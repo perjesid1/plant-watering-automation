27f758312b391d77ca57fe967742f18332a1f1e7
Gabor512 <gyarmatigabor512@gmail.com>
2025-12-14 20:25:47 +0100

    Prepare HiveMQ Cloud MQTT config

--- src/config.py
+++ src/config.py
@@ -29,7 +29,11 @@ RTC_TIMESTAMP_SIZE: int = 8 # How many bites the timestamp takes up in the RTC m
 MSG_REPEAT: int = 3 # How many times do we repeat the messages on the OLED screen.
 
 # --- MQTT CONFIG (public) ---
-MQTT_BROKER: str = "192.168.1.100"
-MQTT_PORT: int = 1883
+MQTT_BROKER: str = "8fb34422df8e460fa89ea4ee5fbd28bf.s1.eu.hivemq.cloud"
+MQTT_PORT: int = 8883
 MQTT_CLIENT_ID: str = "plant_device_01"
 MQTT_TOPIC_BASE: str = "plant/device01"
+
+
+# TLS settings
+MQTT_USE_TLS: bool = True 
